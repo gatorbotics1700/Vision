60717ec19fd83e17c93660116f6c94901d7c5ec5
Cantevisa <18vwoo@castilleja.org>
2016-06-22 11:57:10 -0700

    Still some character errors...

--- AndroidApp/app/src/main/java/com/example/katiemishra/visionapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/katiemishra/visionapp/MainActivity.java
@@ -5,37 +5,26 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.VideoView;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
-    ​
+public class MainActivity extends AppCompatActivity {​
     ImageView iv;
     ​
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
-        ​
-        iv = (ImageView) findViewById(R.id.myImageView);
-        ​
+        setContentView(R.layout.content_main);​
+        iv = (ImageView) findViewById(R.id.myImageView);​
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 0);
         Mat m = new Mat();
-    }
-    ​
+    }​
     static final int REQUEST_VIDEO_CAPTURE = 1;
     ​
     @Override
@@ -45,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
-            ​
             // convert uri to mat
             try {
                 bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
@@ -53,12 +41,10 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            // processing goes here
-            ​
+            // processing goes here​
             // mat to bitmap
             Bitmap bm = Bitmap.createBitmap(m.cols(), m.rows(),Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(m, bm);
-            ​
+            Utils.matToBitmap(m, bm);​
             iv.setImageBitmap(bm);
         }
     }
